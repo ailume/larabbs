55e0cbc298fc7f185cbbf70a64d8fe8f24ce31d3
vagrant <vagrant@homestead>
2019-09-10 15:38:52 +0000

    分类模型和数据

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,7 @@ class UsersController extends Controller
     //
     public function show(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.show', compact('user'));
     }
 
